0d6655c72178fd9680dcf427953ffa1a712770b4
Hailes Maurício <hailes.antonio@mastercontas.com>
2022-05-08 02:03:53 +0100

    fix menu strip

--- Controles/UserControlDay.Designer.cs
+++ Controles/UserControlDay.Designer.cs
@@ -29,7 +29,15 @@ namespace TaLigado.Controles
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControlDay));
             this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.agendarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.limparEventosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -43,6 +51,53 @@ namespace TaLigado.Controles
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(69)))));
+            this.contextMenuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agendarToolStripMenuItem,
+            this.limparEventosToolStripMenuItem,
+            this.visualizarToolStripMenuItem,
+            this.imprimirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 114);
+            // 
+            // agendarToolStripMenuItem
+            // 
+            this.agendarToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.agendarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("agendarToolStripMenuItem.Image")));
+            this.agendarToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.agendarToolStripMenuItem.Name = "agendarToolStripMenuItem";
+            this.agendarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.agendarToolStripMenuItem.Text = "Novo Evento";
+            this.agendarToolStripMenuItem.Click += new System.EventHandler(this.agendarToolStripMenuItem_Click);
+            // 
+            // limparEventosToolStripMenuItem
+            // 
+            this.limparEventosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.limparEventosToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("limparEventosToolStripMenuItem.Image")));
+            this.limparEventosToolStripMenuItem.Name = "limparEventosToolStripMenuItem";
+            this.limparEventosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.limparEventosToolStripMenuItem.Text = "Limpar Eventos";
+            // 
+            // visualizarToolStripMenuItem
+            // 
+            this.visualizarToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.visualizarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("visualizarToolStripMenuItem.Image")));
+            this.visualizarToolStripMenuItem.Name = "visualizarToolStripMenuItem";
+            this.visualizarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.visualizarToolStripMenuItem.Text = "Visualizar";
+            // 
+            // imprimirToolStripMenuItem
+            // 
+            this.imprimirToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.imprimirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("imprimirToolStripMenuItem.Image")));
+            this.imprimirToolStripMenuItem.Name = "imprimirToolStripMenuItem";
+            this.imprimirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.imprimirToolStripMenuItem.Text = "Imprimir";
+            // 
             // UserControlDay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -52,6 +107,10 @@ namespace TaLigado.Controles
             this.Name = "UserControlDay";
             this.Size = new System.Drawing.Size(99, 49);
             this.Load += new System.EventHandler(this.UserControlDay_Load);
+            this.Click += new System.EventHandler(this.UserControlDay_Click);
+            this.DoubleClick += new System.EventHandler(this.UserControlDay_DoubleClick);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UserControlDay_MouseClick);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +119,10 @@ namespace TaLigado.Controles
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem agendarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem limparEventosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visualizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirToolStripMenuItem;
     }
 }
--- Controles/UserControlDay.cs
+++ Controles/UserControlDay.cs
@@ -24,5 +24,36 @@ namespace TaLigado.Controles
             label1.Text = day.ToString();
             return this;
         }
+
+        private void agendarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControlDay_DoubleClick(object sender, EventArgs e)
+        {
+            //this.Controls.Add(contextMenuStrip1);
+            //contextMenuStrip1.Parent = this;
+            //contextMenuStrip1.Show();
+        }
+
+        private void UserControlDay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControlDay_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(PointToScreen(e.Location));
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                var cor = this.BackColor;
+                this.BackColor = this.label1.ForeColor;
+                this.label1.ForeColor = cor;
+            }
+        }
     }
 }
